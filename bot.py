062854020facc3a24b6f815a79c908588fa1d5dd
Reed Chan <reedchan@users.noreply.github.com>
2017-03-13 19:08:17 -0400

    Added WIP bot based on discord.py's basic_bot Added setup Python script for generating a dictionary of Terraria's prefixes

---
+++ bot.py
@@ -0,0 +1,95 @@
+import discord
+import json
+import random
+from discord.ext import commands
+from sys import argv
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+bot = commands.Bot(command_prefix='!', description=description)
+prefixDict = None
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command(pass_context=True)
+async def add(ctx, left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say("%d + %d = %d" % (left, right, left + right))
+
+@bot.command()
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(*choices : str):
+    """Chooses between multiple choices."""
+    await bot.say(random.choice(choices))
+
+@bot.command()
+async def repeat(times : int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await bot.say(content)
+
+@bot.command()
+async def joined(member : discord.Member):
+    """Says when a member joined."""
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+@bot.group(pass_context=True)
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+@cool.command(name='bot')
+async def _bot():
+    """Is the bot cool?"""
+    await bot.say('Yes, the bot is cool.')
+    
+@bot.command()
+async def terraria(search: str):
+  prefix = search.lower()
+  if (prefix in prefixDict):
+    id = prefixDict[prefix]
+    if (prefix in ("deadly", "hasty", "quick")):
+      await bot.say("```%s: %s, %s```" % (prefix, id[0], id[1]))
+    else:
+      await bot.say("```%s: %s```" % (prefix, id))
+  else:
+    await bot.say("```Please specify the prefix you would like to look up.```")
+
+def main(argv):
+  global prefixDict
+  try:
+    f = open("terrariaPrefixes.json", 'r')
+    prefixDict = json.loads(f.read())
+    f.close()
+  except Exception as e:
+    print(e)
+    sys.exit(1)
+  try:
+    assert(len(argv) > 1)
+  except AssertionError:
+    print("Please specicfy the bot's token as an argument.")
+    sys.exit(1)
+  bot.run(argv[1])
+
+if __name__ == '__main__':
+  main(argv)
---
+++ setup.py
@@ -0,0 +1,134 @@
+import getopt
+import io
+import json
+import urllib.error
+import urllib.request
+from bs4 import BeautifulSoup
+from os import path
+from sys import argv, exit
+
+# Import ElementTree as etree to parse the HTML tables for the prefixes
+try:
+  from defusedxml import ElementTree as etree
+except ImportError:
+  print("Please install defusedxml to safely parse the HTML tables.")
+  from xml.etree import ElementTree as etree
+
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 10.0; rv:10.0) Gecko/20100101 Firefox/52.0'
+sendHeader={'User-Agent':user_agent,}
+
+# Dictionary for Terraria prefix IDs
+prefixDict = dict()
+# Dictionary for the national Pokédex
+nationalDex = dict()
+
+def help(returnCode):
+  info = """\
+Usage: %s [options...]
+  -a, --all             Perform all setup operations
+  -h, --help            Print this help message
+  -p, --pokemon         Setup for Pokémon
+  -t, --terraria        Setup for Terraria prefixes
+""" % path.split(__file__)[1]
+  print(info)
+  exit(returnCode)
+
+# Send a GET request to targetURL, read the data from the response, and return
+# it as a BeautifulSoup object
+# Print errorMsg if there is an exception raised by urllib or by BeautifulSoup
+def getSoup(targetURL, errorMsg):
+  try:
+    req = urllib.request.Request(url=targetURL, data=None, headers=sendHeader)
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    soup = BeautifulSoup(data, "html.parser")
+  except Exception as e:
+    if (errorMsg != ""):
+      print(errorMsg)
+    print(e)
+    exit(1)
+  return soup
+
+# Save writeDict by dumping it to a string in a JSON format and writing it to
+# writeFile
+# Print errorMsg if there is an exception
+def saveDict(writeDict, writeFile, errorMsg):
+  dictString = json.dumps(writeDict, sort_keys=True, indent=2)
+  try:
+    f = open(writeFile, "w")
+    f.write(dictString)
+    f.close()
+  except Exception as e:
+    if (errorMsg != ""):
+      print(errorMsg)
+    print(e)
+    exit(1)
+  return
+
+def main(argv):
+  shortOpts = "ahpt"
+  longOpts = ["all", "help", "pokemon", "terraria"]
+  try:
+    opts, args = getopt.getopt(argv[1:], shortOpts, longOpts)
+  except Exception as e:
+    print(e)
+    help(2)
+  # Whether or not to get the national Pokedex from Bulbapedia
+  pokedex = False
+  # Whether or not to get the prefixes from the official Terraria wiki
+  prefixes = False
+  for (o, a) in opts:
+    if (o in ("-a", "--all")):
+      pokedex = True
+      prefixes = True
+    elif (o in ("-h", "--help")):
+      help(2)
+    elif (o in ("-p", "--pokemon")):
+      pokedex = True
+    elif (o in ("-t", "--terraria")):
+      prefixes = True
+  if ((not pokedex) and (not prefixes)):
+    print("Please specify what you would like to setup.")
+    help(2)
+  if (pokedex):
+    soup = getSoup(targetURL="http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number",
+                   errorMsg="")
+    tables = soup.find_all("table")
+    print(tables[1])
+    # saveDict(writeDict=nationalDex,
+             # writeFile="pokedex.json",
+             # errorMsg="Error writing pokedex to pokedex.json")
+    pass
+  if (prefixes):
+    soup = getSoup(targetURL="http://terraria.gamepedia.com/Prefix_IDs",
+                   errorMsg="")
+    # Get the tables
+    tables = soup.find_all("table")
+    try:
+      assert(len(tables) > 1)
+      assert(tables[1]["class"] == ["terraria", "sortable"])
+    except AssertionError:
+      print("Page layout changed - script must be updated")
+      exit(1)
+    # Get the second table which should contain the prefix table
+    # Next line works for xml.etree.ElementTree and for defusedxml.ElementTree
+    table = etree.XML(str(tables[1]))
+    for row in table:
+      # Convert to lowercase and strip whitespace
+      prefix  = row[0].text.lower().strip()
+      id      = row[1].text.lower().strip()
+      # Ignore the table headers
+      if ((prefix == "prefix") or (id == "value")):
+        continue
+      else:
+        if (prefix in prefixDict):
+          prefixDict[prefix] = [prefixDict[prefix], id]
+        else:
+          prefixDict[prefix] = id
+    saveDict(writeDict=prefixDict,
+             writeFile="terrariaPrefixes.json",
+             errorMsg="Error writing prefixes to terrariaPrefixes.json")
+  return
+
+if __name__ == '__main__':
+  main(argv)
